cdd3580e4c949f16d4b49353e042ae7e27d62cf1
Aner Perez <aner@ncstech.com>
2019-02-28 09:54:16 -0500

    Track jOOQ 3.11 jooq-meta refactoring

--- src/main/java/com/vertabelo/jooq/VertabeloModelLoader.java
+++ src/main/java/com/vertabelo/jooq/VertabeloModelLoader.java
@@ -13,7 +13,7 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.jooq.tools.JooqLogger;
 import org.jooq.tools.StringUtils;
-import org.jooq.util.xml.XMLDatabase;
+import org.jooq.meta.xml.XMLDatabase;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
